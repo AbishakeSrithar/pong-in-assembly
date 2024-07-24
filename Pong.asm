2c43e4927af26dce3c8b4247c46c5d5b7a6c43dd
AbishakeSrithar <sritharabishake@gmail.com>
2024-07-24 18:38:43 +0100

    Setting background colour

--- Pong.asm
+++ Pong.asm
@@ -9,6 +9,39 @@ DATA ENDS
 CODE SEGMENT PARA 'CODE' ; Code Segments contains the actual machine instructions that the CPU executes
 
   MAIN PROC FAR ; name of Procedure and entry point because MAIN name. PROC indicates start. FAR is type of procedure (eg. can be called from any segment in seg mem model? opposite is NEAR)
+    
+    MOV AH, 00h ; Function to set video mode
+    MOV AL, 13h ; https://mendelson.org/wpdos/videomodes.txt video mode: 320x200 256 colour
+    INT 10h ; the 17th (hexadec) interrupt vector (executes the function above)
+
+    ; tutorial
+    ; MOV AH, 0Bh ; Set config type to parent of background (display/cursor pos, other vid adjacent)
+    ; MOV BH, 00h ; To the background colour
+    ; MOV BL, 01h ; choose black as background colour
+    ; INT 10h ; executes above
+
+    ; stackoverflow but gives wrong colour/code
+    ; MOV AX,0600h 
+    ; MOV BH, 48h   
+    ; MOV CX,0000h  
+    ; MOV DX,184Fh  
+    ; INT 10h
+
+    ; chatgpt (works)
+    MOV AX, 0A000h     ; Video memory segment
+    MOV ES, AX
+    MOV DI, 0000h      ; Start at the beginning of video memory
+    MOV AL, 04h        ; Color index for red (assuming 04h is red in the current palette)
+    MOV CX, 320 * 200  ; Number of pixels to fill
+    REP STOSB          ; Fill the screen with the color
+
+    MOV AH, 0Ch ; Set the config to writing a pixel
+    MOV AL, 0Fh ; Choose White colour
+    MOV BH, 00h ; Set the page number to 0
+    MOV CX, 0Ah ; Set the column (x)
+    MOV DX, 0Ah ; Set the line (y)
+    INT 10h ; executes above
+    
     RET
   MAIN ENDP ; End of procedure (PROC)
 
